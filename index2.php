8a064d24ec2903a6cfb974bff21774c30724d0a2
Pangu3 <95626474+Pangu3@users.noreply.github.com>
2021-12-15 12:38:23 -0300

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Listar Archivos</title>
+<meta charset="UTF-8">
+</head>
+<body>
+
+<?php 
+//Creamos Nuestra Función
+function lista_archivos(){ //La función recibira como parametro un carpeta
+    $carpeta="./";
+    if (is_dir($carpeta)) { //Comprovamos que sea un carpeta Valido
+        if ($dir = opendir($carpeta)) {//Abrimos el carpeta
+            echo '<ul>';
+            while (($archivo = readdir($dir)) !== false){ //Comenzamos a leer archivo por archivo
+                if ($archivo != '.' && $archivo != '..'){ 
+                    $nuevaRuta = $carpeta.$archivo.'/';
+                    echo "<li><a href='".$archivo."'>"; //Abrimos un elemento de lista 
+                    if (is_dir($nuevaRuta)) { //Si la ruta que creamos es un carpeta entonces:
+                        echo '<b>'.$nuevaRuta.'</b>'; //Imprimimos la ruta completa resaltandola en negrita
+                        lista_archivos($nuevaRuta);//Volvemos a llamar a este metodo para que explore ese carpeta.
+                    } else { //si no es un carpeta:
+                        echo "Archivo: ".$archivo; //simplemente imprimimos el nombre del archivo actual
+                    }
+                     '</a></li>'; //Cerramos el item actual y se inicia la llamada al siguiente archivo
+                }
+            }//finaliza 
+            echo '</ul>';//Se cierra la lista
+            closedir($dir);//Se cierra el archivo
+        }
+    }else{//Finaliza el If de la linea 12, si no es un carpeta valido, muestra el siguiente mensaje
+        echo 'No Existe la carpeta';
+    }				
+}//Fin de la Función	 
+//Llamamos a la función
+lista_archivos();
+?>
+</body>
+</html>
